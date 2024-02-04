5696a6f35ff8af24ff2fa4cd343b2c4b2252e963
adityacodes30 <adityaework@gmail.com>
2024-02-05 00:05:00 +0530

    folders

--- BusinessLogic/main.py
+++ BusinessLogic/main.py
@@ -1,15 +1,42 @@
-from typing import Union
+# from typing import Union
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
+
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=8080))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='catalogQueue', durable=True)
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='catalogQueue', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+main()
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         try:
+#             sys.exit(0)
+#         except SystemExit:
+#             os._exit(0)
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
