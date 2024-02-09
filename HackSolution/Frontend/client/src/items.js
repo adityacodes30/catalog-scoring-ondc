5c4452050f1ce39747110f39a571a157ae5d7ebe
vigkrishna <kvig1_be22@thapar.edu>
2024-02-09 18:21:33 +0530

    backend ...

--- HackSolution/Frontend/client/src/items.js
+++ HackSolution/Frontend/client/src/items.js
@@ -1,13 +1,13 @@
 const catalogItems = [
-    'search_tea',
-    'search_bread',
-    'search_soap',
-    'search_rice',
-    'search_deodrants',
-    'search_shampoo',
-    'search_conditioner',
-    'search_toothpaste',
-    'search_toothbrush',
+    'tea',
+    'bread',
+    'soap',
+     'honey',
+    // 'search_deodrants',
+    'shampoo',
+    'conditioner',
+    'toothpaste',
+    'toothbrush',
   ];
   
   export default catalogItems;
